c339d9c24af5c93977020b69fee68ba45a0745ce
charriotx <siarheysakhonchyk@mail.ru>
2025-04-01 13:03:52 +0300

    hmw 9.1

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -40,7 +40,7 @@ function Clock() {
     }
 
     const stringTime = `${String(date.getHours()).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}` || <br /> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${String(date.toLocaleDateString())}` || <br /> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${String(date.getDate().toLocaleString()).padStart(2, "0")}.${String(date.toLocaleString('en-US', { month: 'numeric' })).padStart(2, "0")}.${String(date.getFullYear())}` || <br /> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = `${date.toLocaleString('en-US', { weekday: 'long' })}` || <br /> // пишут студенты
