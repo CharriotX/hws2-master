ea9c71bf3d8bfe66b0e5f9f358002abd227da690
charriotx <siarheysakhonchyk@mail.ru>
2025-04-30 21:14:37 +0300

    hmw13 1.1

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,24 +36,27 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText("...всё ок) \nкод 200 - обычно означает что скорее всего всё ок)")
+                setText("...всё ок)")
                 // дописать
-                setInfo('')
+                setInfo('код 200 - обычно означает что скорее всего всё ок)')
             })
             .catch((e) => {
                 // дописать
                 if (e.response.status === 500) {
                     setCode('Ошибка 500!')
                     setImage(error500)
-                    setText("эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)")
+                    setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                    setText("эмитация ошибки на сервере")
                 } else if (e.response.status === 400) {
                     setCode('Ошибка 400!')
                     setImage(error400)
-                    setText("Ты не отправил success в body вообще!\nошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!")
+                    setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                    setText("Ты не отправил success в body вообще!")
                 } else {
                     setCode('Error')
                     setImage(errorUnknown)
                     setText("Network Error\nAxiosError")
+                    setInfo('Error')
                 }
                 setInfo('')
             })
